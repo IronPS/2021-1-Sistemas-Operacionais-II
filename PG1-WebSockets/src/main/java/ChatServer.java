48d647d26cf1a3cf456a9abc505396b1abc1de06
Mayra <mayra.cademartori@gmail.com>
2021-09-23 20:32:59 -0300

    onError handler on PG1

--- PG1-WebSockets/src/main/java/ChatServer.java
+++ PG1-WebSockets/src/main/java/ChatServer.java
@@ -2,12 +2,10 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.websocket.OnClose;
-import javax.websocket.OnMessage;
-import javax.websocket.OnOpen;
-import javax.websocket.Session;
+import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
 
+import java.util.concurrent.TimeoutException;
 import java.util.logging.Logger;
 import java.util.logging.Level;
 
@@ -19,17 +17,15 @@ public class ChatServer{
 	@OnOpen
 	public void onOpen(final Session session) {
 		String sessionID = session.getId();
-			
-		if (webSessionMap.containsKey(sessionID)) {
-			webSessionMap.remove(sessionID);
-		} 
+
+		webSessionMap.remove(sessionID);
 
 		webSessionMap.put(sessionID, session);
 		
 		LOGGER.log(Level.INFO, String.format("WebSocket Connect: %s", sessionID));
 		
 		try {
-			session.getBasicRemote().sendText(String.format("Welcome to Session %s!", sessionID));
+			session.getBasicRemote().sendText(String.format("[Welcome to Session %s!]", sessionID));
 		} catch (IOException e) {
 			LOGGER.log(Level.SEVERE, e.getMessage());
 		}
@@ -39,27 +35,31 @@ public class ChatServer{
 	public void onMessage(Session session, String msg) {
 		try {
 			for (Map.Entry<String, Session> entry : webSessionMap.entrySet()) {
-				entry.getValue().getBasicRemote().sendText(msg);
-
+				entry.getValue().getBasicRemote().sendText(String.format("[Session %s says:] %s", session.getId(), msg));
 			}
 		} catch (IOException e) {
 			LOGGER.log(Level.SEVERE, e.getMessage());
 		}
 		
 		LOGGER.log(Level.INFO, String.format("Message sent: %s", msg));
-
 	}
 	
 	@OnClose
 	public void onClose(final Session session) {
 		String sessionID = session.getId();
-		
-		if (webSessionMap.containsKey(sessionID)) {
-			webSessionMap.remove(sessionID);
-				
-		}
+
+		webSessionMap.remove(sessionID);
 		
 		LOGGER.log(Level.INFO, String.format("WebSocket Disconnect: %s", sessionID));
+	}
+
+	@OnError
+	public void onError(Session session, Throwable t) {
+		if (t instanceof TimeoutException) {
+			LOGGER.log(Level.WARNING, String.format("WebSocket error on session %s: TIMEOUT", session.getId()));
+			return;
+		}
+		LOGGER.log(Level.SEVERE, String.format("Websocket error on session %s: %s", session.getId(), t.getCause()));
 	}
 
 }
